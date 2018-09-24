6cba8b355b656c218b94b93da17c686d71c1ff9c
Andres Ochoa <andoch93@gmail.com>
2018-09-24 14:38:56 -0400

    Added city and state views plus comments

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import json
 import urllib2
 
+# takes in user input for artist search. This value is passed into the keyword
+# parameter for ticketmaster's api call
 artistSearch = raw_input('Search for an artist: ')
+print('\n')
 
-# api url from ticketmaster that extracts all events with Haken as a keyword
+# api url from ticketmaster that extracts all events using user input as keyword
 url = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artistSearch + "&countryCode=US&apikey=cEi2G5zNlAiLrkjwXHXusWpMfLTKsy8M"
 
 # loading the json from api call into json object for parsing
@@ -12,8 +15,13 @@ urlJson = json.load(urllib2.urlopen(url))
 # extract all items from the embedded object
 eventsList = urlJson['_embedded']['events']
 
+# prints all relevant event info from json object returned by ticketmaster's api
 for event in eventsList:
     artist = event['name']
     date = event['dates']['start']['localDate']
     venue = event['_embedded']['venues'][0]['name']
-    print(date + ': ' + artist + ' at ' + venue)
+    city = event['_embedded']['venues'][0]['city']['name']
+    state = event['_embedded']['venues'][0]['state']['stateCode']
+    print(date + ': ' + artist + ' at ' + venue + ' in ' + city + ', ' + state)
+
+print('\n')
